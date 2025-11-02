3828320d23312f00b67bf8ef90cecf663d3a380a
jjy12163 <lalalove0310@gmail.com>
2025-11-03 02:27:04 +0900

    Does : 기능 구현 목록 수정

--- src/main/java/lotto/View/OutputView.java
+++ src/main/java/lotto/View/OutputView.java
@@ -1,11 +1,10 @@
 package lotto.View;
 
 import lotto.Model.Lotto;
-import lotto.Model.MyLotto;
 
 import java.util.List;
 
-public class OutputView {
+public class PurchaseOutputView {
     public static void myLottoPrint(Integer lottoAmount, List<Lotto> lottos) {
         System.out.println(lottoAmount + "개를 구매했습니다.");
         for(Lotto lotto : lottos) {
