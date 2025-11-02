37307f5c39521d9bd0bc877d4074cef2eb3d82af
jjy12163 <lalalove0310@gmail.com>
2025-11-02 15:26:35 +0900

    Feat: 구입하는 로또 수량 계산

--- src/main/java/lotto/Controller/LottoController.java
+++ src/main/java/lotto/Controller/LottoController.java
@@ -1,11 +1,14 @@
 package lotto.Controller;
 
+import lotto.Model.MyLotto;
 import lotto.Model.Validator;
-import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Validate;
 
 public class LottoController {
 
+    public static final Integer LottoPrice = 1000;
+
     private final String rawPurchasePrice;
+
 
     public LottoController(String rawPurchasePrice) {
         this.rawPurchasePrice = rawPurchasePrice;
@@ -13,9 +16,16 @@ public class LottoController {
 
     public void run() {
         int price = Validator.validatePurchasePrice(rawPurchasePrice);
+        int lottoAmount = LottoAmount(price);
+        MyLotto myLotto = new MyLotto(lottoAmount);
+        myLotto.buy();
+
     }
 
 
+    private static int LottoAmount(int price) {
+        return price / LottoPrice;
+    }
 
 
 
---
+++ src/main/java/lotto/Model/MyLotto.java
@@ -0,0 +1,16 @@
+package lotto.Model;
+
+public class MyLotto {
+    private final Integer lottoAmount;
+
+    public MyLotto(int lottoAmount) {
+        this.lottoAmount = lottoAmount;
+    }
+
+    public void buy() {
+
+    }
+
+
+
+}
